98edbaaa8b256898bfeede4585c8ceabbc7fa4e5
Aleksander Andre <aleksander@axendo.ee>
2020-11-04 11:15:00 +0200

    Initial commit

---
+++ src/Highmaps.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Highmaps class file.
+ *
+ * @author Milo Schuman <sankaest@gmail.com>
+ * @link https://github.com/sankaest/yii2-highcharts/
+ * @license https://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace sankaest\highcharts;
+
+/**
+ * @see Highcharts
+ */
+class Highmaps extends Highcharts
+{
+
+    protected $constr = 'mapChart';
+    protected $baseScript = 'modules/map';
+}
